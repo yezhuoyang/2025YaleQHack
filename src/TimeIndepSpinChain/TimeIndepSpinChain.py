264823c117d43b6798d55e819b5ed65cb3b9ae05
victortcyu <victortcyu@gmail.com>
2025-04-12 22:30:47 -0400

    Add error models
    Did not fully succeed though :(

--- src/TimeIndepSpinChain/TimeIndepSpinChain.py
+++ src/TimeIndepSpinChain/TimeIndepSpinChain.py
@@ -35,6 +35,31 @@ def extended_opt(self):
 
     return run_pass
 
+def unopt(self):
+    native_rewrite = Walk(RydbergGateSetRewriteRule(self)) # use Kirin's functionality to walk code line by line while applying neutral-atom gate decomposition as defined in Bloqade
+    parallelize_pass = UOpToParallel(self) # review the code and apply parallelization using a heuristic
+    agg_fold = QASM2Fold(self) # supports parallelization by unfolding loops to search for parallelization opportunities
+
+    # here we define our new compiler pass
+    def run_pass(
+        kernel: ir.Method,
+        *,
+        fold: bool = False,
+        typeinfer: bool = False,
+        parallelize: bool = False,
+    ):
+        assert qasm2.extended.run_pass is not None
+        qasm2.extended.run_pass(kernel, fold=fold, typeinfer=typeinfer) # apply the original run_pass to the lowered kernel
+        native_rewrite.rewrite(kernel.code) # decompose all gates in the circuit to neutral atom gate set
+
+        # here goes our parallelization optimizer; the order of the commands here matters!
+        if parallelize:
+            agg_fold.fixpoint(kernel)
+            parallelize_pass(kernel)
+
+    return run_pass
+
+
 def SpinChainLieTrotter(n: int, time: int, steps: int, parallelize: bool = True):
     n_qubits = int(2**n)
     
@@ -69,6 +94,37 @@ def SpinChainLieTrotter(n: int, time: int, steps: int, parallelize: bool = True)
 
     return SpinChainLieTrotter_program
 
+def SpinChainLieTrotterUnopt(n: int, time: int, steps: int):
+    n_qubits = int(2**n)
+    
+    def trotter_layer(qreg: qasm2.QReg, timestep: int, J: int, h: int):
+        for i in range(n_qubits):
+            qasm2.cx(qreg[i], qreg[(i+1)%n_qubits])
+            qasm2.rz(qreg[(i+1)%n_qubits], 2*J*timestep)
+            qasm2.cx(qreg[i], qreg[(i+1)%n_qubits])
+        for i in range(n_qubits):
+            qasm2.rx(qreg[i], 2*h*timestep)
+
+    def SpinChainLieTrotterUnopt_program():
+        if time == 0: 
+            creg = qasm2.creg(n_qubits) 
+            return creg
+        
+        qreg = qasm2.qreg(n_qubits)
+        creg = qasm2.creg(n_qubits)
+
+        J = 0.2 
+        h = 1.2 
+        timestep = time/steps 
+        for i in range(steps): 
+            trotter_layer(qreg, timestep, J, h)
+        
+        for i in range(n_qubits):
+            qasm2.measure(qreg[i],creg[i])
+
+        return creg
+
+    return SpinChainLieTrotterUnopt_program
 
 def SpinChainLieTrotterParallel(n: int, time: int, steps: int, parallelize: bool = True):
     n_qubits = int(2**n)
